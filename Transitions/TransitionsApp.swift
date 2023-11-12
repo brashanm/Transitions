fe91e45991d41762cd1d4f7e5ad19e33ad505e22
Brashan <brashanm@gmail.com>
2023-11-11 22:50:58 -0500

    Initial Commit

---
+++ Transitions/TransitionsApp.swift
@@ -0,0 +1,17 @@
+//
+//  TransitionsApp.swift
+//  Transitions
+//
+//  Created by Brashan Mohanakumar on 2023-11-11.
+//
+
+import SwiftUI
+
+@main
+struct TransitionsApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
